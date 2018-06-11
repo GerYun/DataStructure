74f15ed69b7f46fa01e7bcf8919900ab6c699cd6
GerYun <duweiwu@DuWeiWudeMacBook-Pro.local>
2018-06-11 21:45:29 +0800

    ...

--- 二叉树遍历/main.c
+++ 二叉树遍历/main.c
@@ -1,8 +1,8 @@
 //
 //  main.c
-//  DymicStack
+//  二叉树遍历
 //
-//  Created by DuWeiWu on 2018/6/9.
+//  Created by DuWeiWu on 2018/6/11.
 //  Copyright © 2018年 DuWeiWu. All rights reserved.
 //
 
@@ -11,113 +11,128 @@
 #include <stdlib.h>
 #include "stdbool.h"
 
+/*假设如下二叉树
+    A
 
-// 动态栈使用链表实现
-typedef struct Node {
-    struct Node * pNext;
-    int data;
-}NODE, *PNODE;
+B       C
+      D   E
+            F
+ */
 
-typedef struct Stack {
-    PNODE pTop;
-    PNODE pBottom;
-}STACK, *PSTACK;
+struct BinaryTree {
+    char data;
+    struct BinaryTree *leftChild;
+    struct BinaryTree *rightChild;
+};
 
-// 初始化一个栈
-PSTACK init(void);
-// 入栈
-void push(PSTACK,int);
-// 出栈
-bool pop(PSTACK,int *);
-// 判断栈是否为空
-bool isEmpty(PSTACK);
-// 清除所有元素
-void clearStack(PSTACK);
-// 遍历输出所有元素
-void traverser(PSTACK);
-
+// 初始化一个二叉树
+struct BinaryTree *create(void);
+// 先序遍历
+void preTraversal(struct BinaryTree *);
+// 中序遍历
+void midTraversal(struct BinaryTree *);
+//后序遍历
+void lastTraversal(struct BinaryTree *);
 
 int main(int argc, const char * argv[]) {
     
-    PSTACK pst = init();
-    push(pst, 1);
-    push(pst, 2);
-    push(pst, 3);
-    push(pst, 4);
-    push(pst, 5);
-    traverser(pst);
-    
-    int val;
-    pop(pst, &val);
-    printf("弹出的值是 %d \n",val);
-    
-    traverser(pst);
-    clearStack(pst);
-    traverser(pst);
+    struct BinaryTree *pTree = create();
+    preTraversal(pTree);
+    printf("\n");
+    midTraversal(pTree);
+    printf("\n");
     
     return 0;
 }
 
 
-PSTACK init(void) {
-    PSTACK pst = malloc(sizeof(STACK));
-    PNODE node = malloc(sizeof(NODE));
-    pst->pBottom = node;
-    pst->pTop = pst->pBottom;
-    return pst;
-}
-
-bool isEmpty(PSTACK pst) {
-    return (pst->pTop == pst->pBottom);
-}
-
-void push(PSTACK pst,int val) {
-    PNODE node = malloc(sizeof(NODE));
-    node->data = val;
-    node->pNext = pst->pTop;
-    pst->pTop = node;
-}
-
-bool pop(PSTACK pst,int *e) {
+struct BinaryTree *create() {
     
-    if (isEmpty(pst)) {
-        return false;
+    struct BinaryTree *pTree = (struct BinaryTree *)malloc(sizeof(struct BinaryTree));
+    struct BinaryTree *pB = (struct BinaryTree *)malloc(sizeof(struct BinaryTree));
+    struct BinaryTree *pC = (struct BinaryTree *)malloc(sizeof(struct BinaryTree));
+    struct BinaryTree *pD = (struct BinaryTree *)malloc(sizeof(struct BinaryTree));
+    struct BinaryTree *pE = (struct BinaryTree *)malloc(sizeof(struct BinaryTree));
+    struct BinaryTree *pF = (struct BinaryTree *)malloc(sizeof(struct BinaryTree));
+    
+    pTree->data = 'A';
+    pTree->leftChild = pB;
+    pTree->rightChild = pC;
+    
+    pB->data = 'B';
+    pB->leftChild = pB->rightChild = NULL;
+    
+    pC->data = 'C';
+    pC->leftChild = pD;
+    pC->rightChild = pE;
+    
+    pD->data = 'D';
+    pD->leftChild = pD->rightChild = NULL;
+    
+    pE->data = 'E';
+    pE->leftChild = NULL;
+    pE->rightChild = pF;
+    
+    pF->data = 'F';
+    pF->rightChild = pF->leftChild = NULL;
+
+    return pTree;
+}
+
+
+void preTraversal(struct BinaryTree *pTree) {
+    
+    if (pTree != NULL) {
+        // 首先操作跟节点。这里的操作就是打印一下
+        printf("%c",pTree->data);
+        
+        //再先序遍历根节点的左子树
+        //而左子树又是一颗子树，所以 递归遍历
+        if (pTree->leftChild != NULL) {
+            preTraversal(pTree->leftChild);
+        }
+        
+        //然后先序遍历根节点的右子树
+        //而右子树又是一颗子树，所以 递归遍历
+        if (pTree->rightChild != NULL) {
+            preTraversal(pTree->rightChild);
+        }
     }
-    
-    PNODE p = pst->pTop;
-    *e = p->data;
-    pst->pTop = pst->pTop->pNext;
-    free(p);
-
-    return true;
 }
 
-
-void clearStack(PSTACK pst) {
-    
-    while (!isEmpty(pst)) {
-        PNODE p = pst->pTop;
-        pst->pTop = pst->pTop->pNext;
-        free(p);
+void midTraversal(struct BinaryTree *pTree) {
+    if (pTree != NULL) {
+        //首先中序遍历根节点的左子树
+        //而左子树又是一颗子树，所以 递归遍历
+        if (pTree->leftChild != NULL) {
+            midTraversal(pTree->leftChild);
+        }
+        
+        // 然后操作根节点。这里的操作就是打印一下
+        printf("%c",pTree->data);
+        
+        //然后zhong序遍历根节点的右子树
+        //而右子树又是一颗子树，所以 递归遍历
+        if (pTree->rightChild != NULL) {
+            midTraversal(pTree->rightChild);
+        }
     }
 }
 
-void traverser(PSTACK pst) {
-    
-    PNODE p = pst->pTop;
-    
-    while (p != pst->pBottom) {
-        printf("%d",p->data);
-        p = p->pNext;
+void lastTraversal(struct BinaryTree *pTree) {
+    if (pTree != NULL) {
+        //首先后序遍历根节点的左子树
+        //而左子树又是一颗子树，所以 递归遍历
+        if (pTree->leftChild != NULL) {
+            midTraversal(pTree->leftChild);
+        }
+        //然后后序遍历根节点的右子树
+        //而右子树又是一颗子树，所以 递归遍历
+        if (pTree->rightChild != NULL) {
+            midTraversal(pTree->rightChild);
+        }
+        
+        // 然后操作根节点。这里的操作就是打印一下
+        printf("%c",pTree->data);
     }
-    
-    printf("\n");
 }
-
-
-
-
-
-
-
-
